b10b4dd82533a7ea0ffe7bb123ca81645dd8fb92
imgeca <imgesucaferoglu@gmail.com>
2024-01-18 11:08:05 +0100

    s

---
+++ models/mart/month_avg_temp.sql
@@ -0,0 +1,25 @@
+WITH temp_daily AS (
+    SELECT *
+    FROM {{ref('prep_temp')}}
+),
+monthly_avg_temp AS (
+    SELECT
+        EXTRACT(MONTH FROM date) AS month,
+        AVG(avgtemp_c) AS avg_monthly_temp
+    FROM temp_daily
+    GROUP BY month
+)
+SELECT *
+FROM monthly_avg_temp;
+
+
+WITH total_avg AS(
+    SELECT 
+    city, country, year, lat, lon,
+    AVG(avgtemp_c) AS avg_temp_month,
+    MAX(maxtemp_c) AS max_temp_month,
+    MIN(mintemp_c) AS min_temp_month
+    FROM {{ref("prep_temp")}}
+    GROUP BY city, country, year, month, lat, lon
+)
+SELECT * FROM total_avg
--- models/mart/weekday_avg_temp.sql
+++ models/mart/weekday_avg_temp.sql
@@ -1,14 +1,10 @@
-WITH temp_daily AS (
-    SELECT *
-    FROM {{ref('staging_weather')}}
-),
-weekday_avg_temp AS (
-    SELECT
-        weekday,
-        AVG(avgtemp_c) AS avg_weekday_temp
-    FROM temp_daily
-    WHERE weekday IS NOT NULL -- Ensure that weekday information is available
-    GROUP BY weekday
+WITH total_avg AS(
+    SELECT 
+    city, country, year, lat, lon,
+    AVG(avgtemp_c) AS avg_temp_week,
+    MAX(maxtemp_c) AS max_temp_week,
+    MIN(mintemp_c) AS min_temp_weekday
+    FROM {{ref("prep_temp")}}
+    GROUP BY city, country, year, week, lat, lon
 )
-SELECT *
-FROM weekday_avg_temp;
+SELECT * FROM total_avg
