e57fd2845f821efac0dcfb6a175418d5837791f2
imgeca <imgesucaferoglu@gmail.com>
2024-01-24 20:33:51 +0100

    a

--- models/mart/month_avg_temp.sql
+++ models/mart/month_avg_temp.sql
@@ -1,10 +1,17 @@
-WITH total_avg_month AS(
+
+
+WITH total_avg_month AS (
     SELECT 
-    city, country, year, lat, lon,
-    AVG(avgtemp_c) AS avg_temp_month,
-    MAX(maxtemp_c) AS max_temp_month,
-    MIN(mintemp_c) AS min_temp_month
+        city, 
+        country, 
+        year, 
+        month,  -- Include 'month' here
+        lat, 
+        lon,
+        AVG(avgtemp_c) AS avg_temp_month,
+        MAX(maxtemp_c) AS max_temp_month,
+        MIN(mintemp_c) AS min_temp_month
     FROM {{ref("prep_temp")}}
     GROUP BY city, country, year, month, lat, lon
 )
-SELECT * FROM total_avg_month
+SELECT * FROM total_avg_month;
--- models/mart/weekday_avg_temp.sql
+++ models/mart/weekday_avg_temp.sql
@@ -1,10 +1,10 @@
 WITH total_avg_week AS(
     SELECT 
-    city, country, year, lat, lon,
+    city, country, year, week, lat, lon,
     AVG(avgtemp_c) AS avg_temp_week,
     MAX(maxtemp_c) AS max_temp_week,
     MIN(mintemp_c) AS min_temp_weekday
     FROM {{ref("prep_temp")}}
     GROUP BY city, country, year, week, lat, lon
 )
-SELECT * FROM total_avg_week
+SELECT * FROM total_avg_week;
